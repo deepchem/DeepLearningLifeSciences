dce775a045e867abcadb57a3408ad719046205aa
miaecle <zqwu@stanford.edu>
2019-07-15 14:44:59 -0700

    fix bug

--- Chapter08/run.py
+++ Chapter08/run.py
@@ -12,7 +12,7 @@ import logging
 from model import DRModel, DRAccuracy, ConfusionMatrix, QuadWeightedKappa
 from data import load_images_DR
 
-RETRAIN = False
+RETRAIN = True
 
 train, valid, test = load_images_DR(split='random', seed=123)
 # Define and build model
@@ -26,6 +26,7 @@ if not os.path.exists('./test_model'):
   os.mkdir('test_model')
 model.build()
 if not RETRAIN:
+  os.system("sh get_pretrained_model.sh")
   model.restore(checkpoint="./test_model/model-84384")
 metrics = [
     dc.metrics.Metric(DRAccuracy, mode='classification'),
@@ -35,17 +36,16 @@ cm = [dc.metrics.Metric(ConfusionMatrix, mode='classification')]
 
 logger = logging.getLogger('deepchem.models.tensorgraph.tensor_graph')
 logger.setLevel(logging.DEBUG)
-for i in range(10):
-  if RETRAIN:
-    print("About to fit model for 10 epochs")
-    model.fit(train, nb_epoch=10, checkpoint_interval=3512)
-  print("About to start train metrics evaluation")
-  model.evaluate(train, metrics)
-  print("About to start valid metrics evaluation")
-  model.evaluate(valid, metrics)
-  print("About to start valid confusion matrix evaluation")
-  model.evaluate(valid, cm)
-  print("About to start test metrics evaluation")
-  model.evaluate(test, metrics)
-  print("About to start test confusion matrix evaluation")
-  model.evaluate(test, cm)
+if RETRAIN:
+  print("About to fit model for 10 epochs")
+  model.fit(train, nb_epoch=10, checkpoint_interval=1000)
+print("About to start train metrics evaluation")
+model.evaluate(train, metrics)
+print("About to start valid metrics evaluation")
+model.evaluate(valid, metrics)
+print("About to start valid confusion matrix evaluation")
+model.evaluate(valid, cm)
+print("About to start test metrics evaluation")
+model.evaluate(test, metrics)
+print("About to start test confusion matrix evaluation")
+model.evaluate(test, cm)
