53e94d705982c7a5ed59fc91cd3531bb02bc7787
Peter Eastman <peastman@stanford.edu>
2021-04-19 12:40:19 -0700

    Fixed output to mnist example

--- Chapter03/mnist.py
+++ Chapter03/mnist.py
@@ -15,15 +15,17 @@ test_dataset = dc.data.NumpyDataset(x_test, y_test)
 # Build the model.
 
 features = tf.keras.Input(shape=(28, 28, 1))
-conv2d_1 = layers.Conv2D(filters=32, kernel_size=3, activation=tf.nn.relu)(features)
-conv2d_2 = layers.Conv2D(filters=64, kernel_size=3, activation=tf.nn.relu)(conv2d_1)
+conv2d_1 = layers.Conv2D(filters=32, kernel_size=5, activation=tf.nn.relu)(features)
+conv2d_2 = layers.Conv2D(filters=64, kernel_size=5, activation=tf.nn.relu)(conv2d_1)
 flatten = layers.Flatten()(conv2d_2)
 dense1 = layers.Dense(units=1024, activation=tf.nn.relu)(flatten)
 dense2 = layers.Dense(units=10, activation=None)(dense1)
-keras_model = tf.keras.Model(inputs=features, outputs=dense2)
+output = layers.Activation(tf.math.softmax)(dense2)
+keras_model = tf.keras.Model(inputs=features, outputs=[output, dense2])
 model = dc.models.KerasModel(
     keras_model,
     loss=dc.models.losses.SoftmaxCrossEntropy(),
+    output_types=['prediction', 'loss'],
     model_dir='mnist')
 
 # Train the model.
